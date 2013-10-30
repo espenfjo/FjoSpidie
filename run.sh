7debc7ac24b2b19dbb08611725d03c44fadcb5d6
Espen Fjellvær Olsen <espen@mrfjo.org>
2013-10-30 22:01:05 +0100

    Java does something odd with my arguments.. Let bash fix it..

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 PATH=/opt/snort/bin:$PATH
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
-xvfb-run -a java -Djava.library.path=target/lib/ -jar target/com.efo.fjospidie-1.0-SNAPSHOT-jar-with-dependencies.jar $@
+xvfb-run -a java -Djava.library.path=target/lib/ -jar target/com.efo.fjospidie-1.0-SNAPSHOT-jar-with-dependencies.jar ${1+"$@"}
