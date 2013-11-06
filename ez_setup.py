395f3479acd1a277cbf2baea8b8b48163609ee98
Espen Fjellvær Olsen <espen@mrfjo.org>
2013-11-06 17:56:15 +0100

    Add configobj dep

--- ez_setup.py
+++ ez_setup.py
@@ -7,5 +7,5 @@ setup(
     dependency_links = ['https://github.com/espenfjo/browsermob-proxy-py/archive/0.5.1.tar.gz#egg=browsermob-proxy-0.5.1',
 'https://github.com/espenfjo/pylibpcap/archive/0.6.4.1.tar.gz#egg=pylibpcap-0.6.4.1'],
     install_requires=[
-        'selenium', 'pyaml', 'psycopg2', 'browsermob-proxy', 'harpy', 'pydot', 'pylibpcap'],
+        'selenium', 'pyaml', 'psycopg2', 'browsermob-proxy', 'harpy', 'pydot', 'pylibpcap','configobj'],
 )
