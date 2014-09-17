6d06f40316b10942396f3a48734c876b230be00d
Espen Fjellvær Olsen <espen@mrfjo.org>
2014-09-17 14:32:23 +0200

    Update pylibpcap dep

--- ez_setup.py
+++ ez_setup.py
@@ -4,7 +4,7 @@ setup(
     name="FjoSpidie",
     version="2.0",
     packages=find_packages(),
-    dependency_links = ['https://github.com/espenfjo/pylibpcap/archive/0.6.4.1.tar.gz#egg=pylibpcap-0.6.4.1', 'lib/suricatasc'],
+    dependency_links = ['https://github.com/espenfjo/pylibpcap/archive/0.6.4.2.tar.gz#egg=pylibpcap-0.6.4.2', 'lib/suricatasc'],
     install_requires=[
         'selenium', 'pyaml', 'psycopg2', 'browsermob-proxy', 'harpy', 'pydot', 'pylibpcap','configobj','simplejson', 'cidrize'],
 )
