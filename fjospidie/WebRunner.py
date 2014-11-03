cb8ede7cdb6621199350f5f1d2aafb6bd9fd7d3a
Espen Fjellvær Olsen <espen@mrfjo.org>
2014-11-03 12:45:05 +0100

    Sleep two seconds before returning to allow plugins to load

--- fjospidie/WebRunner.py
+++ fjospidie/WebRunner.py
@@ -108,6 +108,7 @@ class WebRunner:
     def analyse_page(self, webdriver, start_url):
         global URLs
         current_page = webdriver.get(start_url.geturl())
+        time.sleep(2)
 
         try:
             screenshot = webdriver.get_screenshot_as_png()
